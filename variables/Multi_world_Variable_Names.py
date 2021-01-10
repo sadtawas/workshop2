024806c9c401de0281e75fa365e9569e5e808e22
sadtawas <zzardguns@gmail.com>
2021-01-10 11:08:58 +0700

    add variable folder and new file_1

---
+++ variables/Multi_world_Variable_Names.py
@@ -0,0 +1,8 @@
+# Camel Case
+myVariableName = "Sadtawas Kudchapant"
+
+# pascal Case
+myVariableName = "Sadtawas Kudchapant"
+
+# Snake Case
+my_variable_name = "Sadtawas Kudcahpant"
---
+++ variables/name.py
@@ -0,0 +1,12 @@
+# สารมารถประกาศได้
+myvar = 'John'
+my_var = 'John'
+_my_var = 'John'
+myVar = 'John'
+MYVAR = 'john'
+myvar2 = 'john'
+
+#ไม่สามารถประกาศได้
+2myvar = 'john'
+my-var = 'john'
+my var = 'john'
